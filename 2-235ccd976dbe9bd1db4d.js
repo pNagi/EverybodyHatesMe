6d4cfc5bff886162e2e82e99663bc48421225311
pNagi <nagisama.j@gmail.com>
2018-10-18 00:57:12 +0700

    Updates

---
+++ 2-235ccd976dbe9bd1db4d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{140:function(n){n.exports={pages:[{componentChunkName:"component---src-pages-index-tsx",jsonName:"index",path:"/"}],dataPaths:{index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E","sq--src-layouts-index-tsx":3728639798}}}}]);
+//# sourceMappingURL=2-235ccd976dbe9bd1db4d.js.map
